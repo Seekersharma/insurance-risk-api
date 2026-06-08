6709d4485082b0bd44f28c7f0a10776751dc05f5
Sushil <sushilsharmanow@gmail.com>
2026-06-07 19:38:57 -0500

    Fix test import order

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,11 +1,10 @@
-from fastapi.testclient import TestClient
-from src.app import app
 import sys
 import os
-
 sys.path.insert(0, os.path.abspath("."))
 
+from fastapi.testclient import TestClient
 from src.app import app
+
 
 client = TestClient(app)
 
