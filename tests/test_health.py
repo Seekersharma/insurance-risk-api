b7e41fa3793e70b7f26c667e8b882bd7bfa77ba0
Sushil <sushilsharmanow@gmail.com>
2026-06-07 19:27:37 -0500

    Fixed CI YML syntax

--- tests/test_health.py
+++ tests/test_health.py
@@ -2,7 +2,10 @@ from fastapi.testclient import TestClient
 from src.app import app
 import sys
 import os
-sys.path.insert(0, os.path.abspath(".."))
+
+sys.path.insert(0, os.path.abspath("."))
+
+from src.app import app
 
 client = TestClient(app)
 
--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,5 +1,11 @@
 from fastapi.testclient import TestClient
 from src.app import app
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("."))
+
+from src.app import app
 client = TestClient(app)
 
 def test_prediction(): 
